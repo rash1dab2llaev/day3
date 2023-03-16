768656d0dec9a464535525141eb24a0b433a868f
Rashid <rash1d.ab2llaev@gmail.com>
2023-03-16 14:56:15 +0500

    аа

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+/* function recurse(num) {
+    if (num == 5) {
+        return num
+    } else {
+        return num + recurse(num + 1)
+    }
+}
+console.log(recurse(1)); */
 
-console.log(a);
-let a='hello'
+/* 
+let k = 0
+let out = ' '
+function recurse() {
+    k++
+    out += k + ' '
+    if (k > 10) {
+        return
+    }
+
+    recurse()
+}
+recurse()
+
+console.log(out); */
+
+/* 
+function fact(num) {
+    if (num == 5) {
+        return num
+    } else {
+        return num * fact(num + 1)
+    }
+}
+console.log(fact(1)); */
+
+
+function NDO(num1) {
+    
+}
+NDO(28)
